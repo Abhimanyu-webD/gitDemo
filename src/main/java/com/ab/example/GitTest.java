4e2240e398788a91b081ee6d5998b7c3c1be09cc
Abhimanyu Gupta <abhigupta,webd@gmail.com>
2019-05-23 20:50:37 +0530

    add new sout

--- src/main/java/com/ab/example/GitTest.java
+++ src/main/java/com/ab/example/GitTest.java
@@ -4,7 +4,8 @@ public class GitTest {
 
 	public static String greet(String name) {
 		if(name=="abhi") {
-			System.out.println("this is my name");
+			System.out.println("this is my name ");
+			System.out.println("It is testing only");
 		
 		return "Hello my dear "+name;
 		}
