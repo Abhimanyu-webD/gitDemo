2e77c8c9bc2146463010b0ed159ca78ee7866b3c
Abhimanyu Gupta <abhigupta,webd@gmail.com>
2019-05-23 19:14:16 +0530

    add if condition

--- src/main/java/com/ab/example/GitTest.java
+++ src/main/java/com/ab/example/GitTest.java
@@ -3,6 +3,12 @@ package com.ab.example;
 public class GitTest {
 
 	public static String greet(String name) {
+		if(name=="abhi") {
+			System.out.println("this is my name");
+		
 		return "Hello my dear "+name;
+		}
+		else 
+			return "Hello my dear "+name;
 	}
 }
